25f126eb28cac34eb035f57b1baae2ed10fd4409
Koodaaja Guru <pekka.aalto@sasky.fi>
2021-09-06 14:22:10 +0300

    config-määrityksille oma tiedosto

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+  $config = array(
+    "urls" => array(
+        "baseUrl" => "/~koodaaja/lanify"
+    )
+  );
+
+?>
+
--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
 
+  // Suoritetaan projektin alustusskripti.
+  require_once '../src/init.php';
+
   // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
   // Siistimisen jälkeen osoite /~koodaaja/lanify/tapahtuma?id=1 on 
   // lyhentynyt muotoon /tapahtuma.
-  $request = str_replace('/~koodaaja/lanify','',$_SERVER['REQUEST_URI']);
+  $request = str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
   $request = strtok($request, '?');
 
   // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava 
