5987e52b5eedb4ffaacfb3d3a9dd3a48bc96cd1f
Koodaaja Guru <pekka.aalto@sasky.fi>
2021-09-06 13:58:10 +0300

    Käsittelijän pohjarunko

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+  // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
+  // Siistimisen jälkeen osoite /~koodaaja/lanify/tapahtuma?id=1 on 
+  // lyhentynyt muotoon /tapahtuma.
+  $request = str_replace('/~koodaaja/lanify','',$_SERVER['REQUEST_URI']);
+  $request = strtok($request, '?');
+
+  // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava 
+  // käsittelijä.
+  if ($request === '/' || $request === '/tapahtumat') {
+    echo '<h1>Kaikki tapahtumat</h1>';
+  } else if ($request === '/tapahtuma') {
+    echo '<h1>Yksittäisen tapahtuman tiedot</h1>';
+  } else {
+    echo '<h1>Pyydettyä sivua ei löytynyt :(</h1>';
+  }
+
+?> 
+
