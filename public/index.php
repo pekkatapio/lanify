515de446242353a8e205900766b4d8679f7fc030
Koodaaja Guru <pekka.aalto@sasky.fi>
2021-11-01 20:45:44 +0200

    istunnon aloitus

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+  // Aloitetaan istunnot.
+  session_start();
+
   // Suoritetaan projektin alustusskripti.
   require_once '../src/init.php';
 
@@ -49,7 +52,8 @@
       if (isset($_POST['laheta'])) {
         require_once CONTROLLER_DIR . 'kirjaudu.php';
         if (tarkistaKirjautuminen($_POST['email'],$_POST['salasana'])) {
-          echo "Kirjautuminen ok!";
+          $_SESSION['user'] = $_POST['email'];
+          header("Location: " . $config['urls']['baseUrl']);
         } else {
           echo $templates->render('kirjaudu', [ 'error' => ['virhe' => 'Väärä käyttäjätunnus tai salasana!']]);
         }
