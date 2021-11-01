8d0cf262be301b0ddd265634f9ac16a9e320f35d
Koodaaja Guru <pekka.aalto@sasky.fi>
2021-11-01 21:24:14 +0200

    kirjautumistietojen tulostus

--- src/view/template.php
+++ src/view/template.php
@@ -8,6 +8,16 @@
   <body>
     <header>
       <h1><a href="<?=BASEURL?>">lanify</a></h1>
+      <div class="profile">
+        <?php
+          if (isset($_SESSION['user'])) {
+            echo "<div>$_SESSION[user]</div>";
+            echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+          } else {
+            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+          }
+        ?>
+      </div>
     </header>
     <section>
       <?=$this->section('content')?>
