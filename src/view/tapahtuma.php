eec08aa77992e99d98d13b4021c78cbc2fa95400
Koodaaja Guru <pekka.aalto@sasky.fi>
2021-11-09 14:20:57 +0200

    ilmoittaudu-nappi tapahtumasivulle

--- src/view/tapahtuma.php
+++ src/view/tapahtuma.php
@@ -9,3 +9,16 @@
 <div><?=$tapahtuma['kuvaus']?></div>
 <div>Alkaa: <?=$start->format('j.n.Y G:i')?></div>
 <div>Loppuu: <?=$end->format('j.n.Y G:i')?></div>
+
+<?php
+  if ($loggeduser) {
+    if (!$ilmoittautuminen) {
+      echo "<div class='flexarea'><a href='ilmoittaudu?id=$tapahtuma[idtapahtuma]' class='button'>ILMOITTAUDU</a></div>";
+    } else {
+      echo "<div class='flexarea'>";
+      echo "<div>Olet ilmoittautunut tapahtumaan!</div>";
+      echo "<a href='peru?id=$tapahtuma[idtapahtuma]' class='button'>PERU ILMOITTAUTUMINEN</a>";
+      echo "</div>";
+    }
+  }
+?>
