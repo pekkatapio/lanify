441f2ead245cd6e6bf2279089e303a07bd701587
Koodaaja Guru <pekka.aalto@sasky.fi>
2021-11-16 16:08:22 +0200

    vahvistuksen tarkistus

--- public/index.php
+++ public/index.php
@@ -106,9 +106,15 @@
       if (isset($_POST['laheta'])) {
         require_once CONTROLLER_DIR . 'kirjaudu.php';
         if (tarkistaKirjautuminen($_POST['email'],$_POST['salasana'])) {
-          session_regenerate_id();
-          $_SESSION['user'] = $_POST['email'];
-          header("Location: " . $config['urls']['baseUrl']);
+          require_once MODEL_DIR . 'henkilo.php';
+          $user = haeHenkilo($_POST['email']);
+          if ($user['vahvistettu']) {
+            session_regenerate_id();
+            $_SESSION['user'] = $user['email'];
+            header("Location: " . $config['urls']['baseUrl']);
+          } else {
+            echo $templates->render('kirjaudu', [ 'error' => ['virhe' => 'Tili on vahvistamatta! Ole hyvä, ja vahvista tili sähköpostissa olevalla linkillä.']]);
+          }
         } else {
           echo $templates->render('kirjaudu', [ 'error' => ['virhe' => 'Väärä käyttäjätunnus tai salasana!']]);
         }
