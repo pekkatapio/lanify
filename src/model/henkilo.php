c5bb5f4e4c9dfabd3a3ae7625d7d4ff9ebb58da8
Koodaaja Guru <pekka.aalto@sasky.fi>
2023-02-15 09:23:46 +0200

    vaihtoavaimen lisääminen henkilo-tauluun

--- src/model/henkilo.php
+++ src/model/henkilo.php
@@ -23,4 +23,16 @@
     return DB::run('UPDATE henkilo SET vahvistettu = TRUE WHERE vahvavain = ?', [$avain])->rowCount();
   }
 
+  function asetaVaihtoavain($email,$avain) {
+    return DB::run('UPDATE henkilo SET nollausavain = ?, nollausaika = NOW() + INTERVAL 30 MINUTE WHERE email = ?', [$avain,$email])->rowCount();
+  }
+
+  function tarkistaVaihtoavain($avain) {
+    return DB::run('SELECT nollausavain, nollausaika-NOW() AS aikaikkuna FROM henkilo WHERE nollausavain = ?', [$avain])->fetch();
+  }
+
+  function vaihdaSalasanaAvaimella($salasana,$avain) {
+    return DB::run('UPDATE henkilo SET salasana = ?, nollausavain = NULL, nollausaika = NULL WHERE nollausavain = ?', [$salasana,$avain])->rowCount();
+  }
+
 ?>
