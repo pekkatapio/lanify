68e1a65f34561a1271f08b6e5255437b34d7dec8
Koodaaja Guru <pekka.aalto@sasky.fi>
2024-05-14 15:59:52 +0300

    lisää ylläpitolinkin kirjautumistietoihin

--- public/index.php
+++ public/index.php
@@ -111,6 +111,7 @@
           if ($user['vahvistettu']) {
             session_regenerate_id();
             $_SESSION['user'] = $user['email'];
+            $_SESSION['admin'] = $user['admin'];
             header("Location: " . $config['urls']['baseUrl']);
           } else {
             echo $templates->render('kirjaudu', [ 'error' => ['virhe' => 'Tili on vahvistamatta! Ole hyvä, ja vahvista tili sähköpostissa olevalla linkillä.']]);
--- src/view/template.php
+++ src/view/template.php
@@ -13,6 +13,9 @@
           if (isset($_SESSION['user'])) {
             echo "<div>$_SESSION[user]</div>";
             echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+            if (isset($_SESSION['admin']) && $_SESSION['admin']) {
+              echo "<div><a href='admin'>Ylläpitosivut</a></div>";  
+            }
           } else {
             echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
           }
